03ebc77e9c47001d8246b0a581102c5ee9e701ca
velan-07 <velan00777@gmail.com>
2025-01-10 18:16:29 +0530

    requirements added

--- webhost/webhost/settings.py
+++ webhost/webhost/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-idh%hr6zpz9m_q0(kxo0)dcv#co!()@i)gj@gb^ud9ifzqae#0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
